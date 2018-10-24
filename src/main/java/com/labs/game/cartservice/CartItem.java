53bf227e0e7d36d04049845baf7d6b751c918180
SIMONE Romano <simone.romano1@ibm.com>
2018-10-24 15:03:23 +0200

    updated test and code...

--- src/main/java/com/labs/game/cartservice/CartItem.java
+++ src/main/java/com/labs/game/cartservice/CartItem.java
@@ -4,13 +4,22 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@Entity(name = "cartitems")
 public class CartItem {
+    @Id
     private Integer id;
+    @Column
     private String customer;
+    @Column
     private String product;
+    @Column
     private int quantity;
 
     public CartItem(int i, String s, String aaa, int i1) {
@@ -18,5 +27,37 @@ public class CartItem {
         this.customer = s;
         this.product = aaa;
         this.quantity = i1;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(String customer) {
+        this.customer = customer;
+    }
+
+    public String getProduct() {
+        return product;
+    }
+
+    public void setProduct(String product) {
+        this.product = product;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
     }
 }
--- src/test/java/com/labs/game/cartservice/CartServiceTest.java
+++ src/test/java/com/labs/game/cartservice/CartServiceTest.java
@@ -1,13 +1,64 @@
 package com.labs.game.cartservice;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.junit.Before;
+import static org.assertj.core.api.Assertions.extractProperty;
+import static org.assertj.core.api.Assertions.assertThat;
+import org.mockito.Mock;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.ArgumentMatchers.eq;
 
 import static org.junit.Assert.*;
 
+@RunWith(MockitoJUnitRunner.class)
 public class CartServiceTest {
 
+    private CartService service;
+
     @Test
-    public void findByCustomer() {
-        assertFalse("Not yet implemented", true);
+    public void findByCustomer_returnsMultipleProducts() {
+        final CartItem[] cartItems = service.findByCustomer("111");
+        assertThat(cartItems).isNotNull();
+        assertThat(cartItems.length).isEqualTo(2);
+        assertThat(extractProperty("product").from(cartItems)).contains("AAA", "BBB");
+
+    }
+
+    @Test
+    public void findByCustomer_returnsSingleProduct() {
+        final CartItem[] cartItems = service.findByCustomer("222");
+        assertThat(cartItems).isNotNull();
+        assertThat(cartItems.length).isEqualTo(1);
+        assertThat(cartItems[0].getCustomer()).isEqualTo("222");
+
+    }
+
+    @Test
+    public void findByCustomer_returnsEmptyArray() {
+        final CartItem[] cartItems = service.findByCustomer("333");
+        assertThat(cartItems).isNotNull();
+        assertThat(cartItems.length).isEqualTo(0);
+    }
+
+    @Mock
+    private CartRepository repository;
+
+    @Before
+    public void setUp() {
+        service = new CartService(repository);
+        given(repository.findByCustomer(eq("111"))).willReturn(
+                new CartItem[]{
+                        new CartItem(0, "111", "AAA", 10),
+                        new CartItem(0, "111", "BBB", 100)
+                });
+
+        given(repository.findByCustomer(eq("222"))).willReturn(
+                new CartItem[]{
+                        new CartItem(0, "222", "AAA", 10)
+                });
+
+        given(repository.findByCustomer(eq("333"))).willReturn(null);
     }
 }
