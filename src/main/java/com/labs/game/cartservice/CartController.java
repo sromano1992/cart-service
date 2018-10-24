dd1f0242f41f947ec09163e234a750a533e08f84
SIMONE Romano <simone.romano1@ibm.com>
2018-10-24 13:21:01 +0200

    added controller and integration test classes...

---
+++ src/main/java/com/labs/game/cartservice/CartController.java
@@ -0,0 +1,13 @@
+package com.labs.game.cartservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CartController {
+
+    @GetMapping("/cart-items")
+    public CartItem[] getCartItems(){
+        return new CartItem[0];
+    }
+}
